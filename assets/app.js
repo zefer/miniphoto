ba702685a55933c12a7c187f4812fa0197f2a0e9
Joe Roberts <joe@zefer.co.uk>
2016-07-19 21:11:49 +0100

    Add an auto-slideshow
    This may not be needed, so keeping it super simple for now.

--- assets/app.js
+++ assets/app.js
@@ -1,14 +1,30 @@
+// Enable an auto-slideshow by adding interval secs to the URL, e.g. `&auto=4`.
+window.onhashchange = function() {
+  var matches = location.hash.match(new RegExp('auto=([^&]*)'));
+  var auto = matches ? matches[1] : null;
+  if(window.slideInterval != undefined) {
+    clearInterval(window.slideInterval)
+  }
+  window.slideInterval = setInterval(function(){
+    try {
+      window.slideshow.next();
+    } catch(e) {
+      clearInterval(window.slideInterval)
+    }
+  }, auto*1000);
+}
+
 function zoom(imageIndex) {
   var pswpElement = document.querySelectorAll('.pswp')[0];
   var options = {
     index: imageIndex,
     shareEl: false
   };
-  var gallery = new PhotoSwipe(
+  window.slideshow = new PhotoSwipe(
     pswpElement,
     PhotoSwipeUI_Default,
     images,
     options
   );
-  gallery.init();
+  slideshow.init();
 }
